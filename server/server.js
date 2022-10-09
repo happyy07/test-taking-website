f0ab002ae6082320d43b0872880b9207340f7f72
Happy Patel <patelhappy267@gmail.com>
2022-10-09 13:36:51 -0400

    added server code for api

--- server/server.js
+++ server/server.js
@@ -1,23 +1,22 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('./server/db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = 3200; // <== You can change the port
 var fs = require('fs');
-var db = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+var db = JSON.parse(fs.readFileSync('./server/db.json', 'utf8'));
 server.use(middlewares);
 
-server.use(router);
-// Add custom routes before JSON Server router
-server.get('/tests', function (req, res) {
-  // See https://github.com/typicode/lowdb
-  let testIds=db.tests.map(elm=>elm.testId)
 
+server.get('/allTests', function (req, res) {
+  // See https://github.com/typicode/lowdb
+  let testIds=db.tests.map(elm=>elm.id)
+  console.log(testIds)
   if (testIds) {
     res.jsonp({testIds})
   } else {
     res.sendStatus(404)
   }
 })
-
+server.use(router);
 server.listen(port);
