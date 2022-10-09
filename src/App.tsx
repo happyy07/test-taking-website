7d8710fa67b6d619cc26099db156b37746608c91
Happy Patel <patelhappy267@gmail.com>
2022-10-09 13:38:29 -0400

    loaded new components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home.component';
-import Test from './components/Test/Test.component';
+import Finish from './components/Finish/Finish.component';
 import Result from './components/Result/Result.component';
+import QOption from './components/QOption/QOption.component';
+import Question from './components/Question/Question.component';
+import QuestionPage from './components/Question/Question.page';
+import Header from './components/Header/Header.component';
+import Footer from './components/Footer/Footer.component';
+import {getAllTests} from './services/APIService';
 
 function App() {
+  const [tests,setTests]=useState([])
+  useEffect(() => {
+    // Update the document title using the browser API
+    getAllTests().then((res)=>{
+      setTests(res.data)
+    })
+  }, []);
   return (
     <div className="App">
-            <h1>Welcome to React Router!</h1>
+      <Header />
       <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Test" element={<Test/>} />
-        <Route path="/Result" element={<Result/>} />
+        <Route path="/" element={<Home tests={tests}/>} />
+        <Route path="/Finish" element={<Finish />} />
+        <Route path="/Result" element={<Result />} />
+        <Route path="/test/:testId/:question" element={<QuestionPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
